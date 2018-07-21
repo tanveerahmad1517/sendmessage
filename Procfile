a21b363b7112a9afefea3f709a2913f63dd75017
tanveerahmad1517 <tanveerobjects@gmail.com>
2018-07-21 11:40:33 +0500

    go live on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pollstr.wsgi
--- pollstr/settings.py
+++ pollstr/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -136,3 +136,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/2fa/'
+
+django_heroku.settings(locals())
